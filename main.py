1048e64297ed56027bc302c0599ebfce8d5518c6
Hunter Quant <hunterdquant@gmail.com>
2016-11-12 13:28:35 -0500

    Added Entity class.

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
 import libtcodpy as libtcod
+from src.entity import Entity
+
+def handle_keys():
+
+    key = libtcod.console_check_for_keypress()
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Alt+Enter: toggle fullscreen
+        libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        return True  #exit game
+
+    global player
+
+    playerx = playery = 0
+
+    #movement keys
+    if libtcod.console_is_key_pressed(libtcod.KEY_UP):
+        playery -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_DOWN):
+        playery += 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_LEFT):
+        playerx -= 1
+
+    elif libtcod.console_is_key_pressed(libtcod.KEY_RIGHT):
+        playerx += 1
+
+    player.move(playerx, playery)
 
 SCREEN_WIDTH = 80
 SCREEN_HEIGHT = 50
@@ -7,10 +37,21 @@ LIMIT_FPS = 20
 libtcod.console_set_custom_font('dejavu10x10_gs_tc.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
 libtcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, 'python/libtcod tutorial', False)
-
+con = libtcod.console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
 libtcod.sys_set_fps(LIMIT_FPS)
 
+player = Entity(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', libtcod.white, con)
+npc = Entity(SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', libtcod.yellow, con)
+objects = [npc, player]
+
 while not libtcod.console_is_window_closed():
-    libtcod.console_set_default_foreground(0, libtcod.white)
-    libtcod.console_put_char(0, 1, 1, '@', libtcod.BKGND_NONE)
+    for object in objects:
+        object.draw()
+    libtcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0)
     libtcod.console_flush()
+
+    for object in objects:
+        object.clear()
+    exit = handle_keys()
+    if exit:
+        break
---
+++ src/entity.py
@@ -0,0 +1,20 @@
+import libtcodpy as libtcod
+
+class Entity:
+    def __init__(self, x, y, char, color, con):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        self.con = con
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        libtcod.console_set_default_foreground(self.con, self.color)
+        libtcod.console_put_char(self.con, self.x, self.y, self.char, libtcod.BKGND_NONE)
+
+    def clear(self):
+        libtcod.console_put_char(self.con, self.x, self.y, ' ', libtcod.BKGND_NONE)
